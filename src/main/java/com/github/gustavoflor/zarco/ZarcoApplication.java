dc190e85d88dbdb6aead897ab82baa9fdb0e710a
gustavo-flor <gustavo.flor@picpay.com>
2022-05-19 09:53:02 -0300

    :tada: Hello Zarco

---
+++ src/main/java/com/github/gustavoflor/zarco/ZarcoApplication.java
@@ -0,0 +1,13 @@
+package com.github.gustavoflor.zarco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarcoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZarcoApplication.class, args);
+    }
+
+}
